8ba333a6b42ae1e5d487ee69dcc7c3d0a59b1aca
raman <raman@fastlite.com>
2021-07-28 12:23:33 +0200

    update setup.py to run smoothly when new Python version is used

--- setup.py
+++ setup.py
@@ -1,62 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy
 from setuptools import setup, Extension
-import sys, os
+from Cython.Build import cythonize
 
-try:
-   import numpy
-except:
-   print('''
--- Error.
-kmpfit requires NumPy, which seems to be unavailable here.
-Please check your Python installation.
-''')
-   sys.exit(1)
-
-
-include_dirs = ["."]
-numdir = os.path.dirname(numpy.__file__)
-ipath = os.path.join(numdir, numpy.get_include())
-include_dirs.append(ipath)
-include_dirs.append('src')
-
-short_descr = "kmpfit"
-
-description = """
-
-    * A function to search for gaussian components in a profile (module
-      profiles) and a class for non-linear least squares curve fitting
-      (module kmpfit)
-   """
-
-define_macros = []
-
-# MS Windows adjustments
-#
-if sys.platform == 'win32':
-    define_macros.append(('YY_NO_UNISTD_H', None))
-    define_macros.append(('_CRT_SECURE_NO_WARNINGS', None))
-
-# avoid using buggy Apple compiler
-#
-if sys.platform=='darwin':
-   from distutils import ccompiler
-   import subprocess
-   import re
-   c = ccompiler.new_compiler()
-   process = subprocess.Popen(c.compiler+['--version'], stdout=subprocess.PIPE)
-   output = process.communicate()[0].strip()
-   version = output.split()[0]
-   if re.match('i686-apple-darwin[0-9]*-llvm-gcc-4.2', version):
-      os.environ['CC'] = 'clang'
+requirements = ['numpy']
 
 setup(
-   name="kmpfit",
-   version="1.0",
-   description=short_descr,
-   long_description=description,
-   platforms = ['Linux', 'Mac OSX', 'Windows'],
-   license = 'BSD',
-   ext_package='kmpfit',
-   packages = ['kmpfit'], 
-   ext_modules = [Extension("kmpfit", sources=["src/kmpfit.c", "src/mpfit.c"],
-                            include_dirs=include_dirs)],
-)
+      name='kmpfit',
+      use_scm_version=dict(version_scheme='{.tag}'.format, local_scheme=lambda vers: '.{}'.format(vers.distance or 0)),
+      setup_requires=['setuptools_scm'],
+      description='kmpfit library',
+      long_description='',
+      install_requires=requirements,
+      include_dirs=[numpy.get_include()],
+      packages=['kmpfit'],
+      ext_modules = cythonize(Extension("kmpfit", ["src/kmpfit.pyx", "src/mpfit.c"]), annotate=False),
+      license = 'BSD',
+      classifiers=[
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'Operating System :: Microsoft :: Windows',
+      'Programming Language :: Python',
+      'Topic :: Scientific/Engineering',
+      'Topic :: Software Development :: Libraries',
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Programming Language :: Python :: 3.6',
+      'Programming Language :: Python :: 3.7',
+      'Programming Language :: Python :: 3.8',
+      'Programming Language :: Python :: 3.9',
+      ],
+   )
